78446ccc70539bf22fa305b793779c67bac4beb1
Andy Chang <yuhaoz1991@gmail.com>
2024-09-15 15:49:00 -0700

    Add Github and google auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,30 +1,40 @@
-
-import type { NextAuthConfig } from "next-auth"
+import type {NextAuthConfig} from "next-auth"
 import Credentials from "@auth/core/providers/credentials";
 import {LoginSchema} from "@/schemas";
 import {getUserByEmail, getUserById} from "@/data/user";
 import bcrypt from "bcryptjs";
+import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-    providers: [Credentials({
-        async authorize(credentials) {
-            const validatedFields = LoginSchema.safeParse(credentials);
-            if(validatedFields.success) {
-               const {email, password} = validatedFields.data;
-               const user = await getUserByEmail(email);
-               if( !user || !user.password){
-                   return null;
-               }
-               const passwordsMatch = await bcrypt.compare(
-                   password,
-                   user.password
-               );
-                if(passwordsMatch){
-                     return user;
+    providers: [
+        Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }),
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+                if (validatedFields.success) {
+                    const {email, password} = validatedFields.data;
+                    const user = await getUserByEmail(email);
+                    if (!user || !user.password) {
+                        return null;
+                    }
+                    const passwordsMatch = await bcrypt.compare(
+                        password,
+                        user.password
+                    );
+                    if (passwordsMatch) {
+                        return user;
+                    }
                 }
+                return null;
             }
-            return null;
-        }
-    })],
+        })],
 } satisfies NextAuthConfig
